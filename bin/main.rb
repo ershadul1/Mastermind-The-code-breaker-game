8fef05544fb073fd1fec57a85f4ba7f32b114f09
ershadul1 <ershadulrayhan@gmail.com>
2020-07-31 09:00:59 +0600

    Add random_array_generator method

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
 
 puts "Hello, Rayhan"
+
+letters = %w[A B C D E F]
+
+
+
+def random_array_generator(available_items, length_of_generated_array)
+  arr = []
+  len = available_items.length - 1
+  length_of_generated_array.times { arr << available_items[rand(0..len)] }
+  arr
+end
+
+p random_array_generator(letters, 6)
