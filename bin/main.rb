ff9c700df94dee74f694883fa362d7896c1865e3
ershadul1 <ershadulrayhan@gmail.com>
2020-07-31 09:17:48 +0600

    Add while loop for taking user input

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,7 @@
 
 puts "Hello, Rayhan"
 
-letters = %w[A B C D E F]
+letters = %w[a b c d e f]
 
 
 
@@ -13,4 +13,20 @@ def random_array_generator(available_items, length_of_generated_array)
   arr
 end
 
-p random_array_generator(letters, 6)
+arr =  random_array_generator(letters, 4)
+
+p arr 
+
+i = 0
+
+while i < 12
+  player_input = []
+  puts "Input four values one by one (press enter after typing each value)"
+  (0..3).each do |i|
+    player_input[i] = gets.chomp
+  end
+
+  p player_input
+  i += 1
+end
+    
